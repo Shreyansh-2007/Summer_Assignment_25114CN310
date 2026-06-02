ee657096d0353f163d97de799ecf0377cfc04961
Shreyansh-2007 <shreyansh70500@gmail.com>
2026-06-02 20:45:31 +0530

    Day-1 uploaded

--- Day-1/Que-4.c
+++ Day-1/Que-4.c
@@ -2,16 +2,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,count=0;
+    int n, count = 0;
     printf("Enter a number : ");
     scanf("%d", &n); //input number
     if(n == 0)
     {
-        printf("Total digits are : 0");
+        printf("Total digits are : 1");
     }    else    {
     while(n != 0)
-    {
-        r = n % 10;
+    {        
         n = n / 10;
         count++;
     }
