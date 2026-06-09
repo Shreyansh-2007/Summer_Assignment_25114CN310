35e3ad3b79a4516e620b723071e50e724fa51b28
Shreyansh-2007 <shreyansh70500@gmail.com>
2026-06-09 20:18:43 +0530

    Day-9 uploaded

---
+++ Day-9/Que-36.c
@@ -0,0 +1,18 @@
+//Write a program to Print hollow square
+#include<stdio.h>
+int main() {
+    int side,i,j;
+    printf("Enter the sides : ");
+    scanf("%d", &side);
+    for(i = 1;i <= side;i++) {
+        for(j = 1;j <= side;j++) {
+            if(i == 1 || i == side || j == 1 || j == side) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }            
+        }
+        printf("\n");
+    }
+    return 0;
+}
