e0f4d96d4009f00003520c6595e683031de70b94
Shreyansh-2007 <shreyansh70500@gmail.com>
2026-06-11 21:54:55 +0530

    Day-11 uploaded

---
+++ Day-11/Que-43.c
@@ -0,0 +1,24 @@
+//Write a program to Write function to check prime
+#include<stdio.h>
+int prime(int n){
+    int i;
+    if(n <= 1) {
+        return 0;
+    }
+    for(i = 2; i <= n / 2; i++) {
+        if(n % i == 0)
+        return 0;
+    }
+    return 1;
+};
+int main() {
+    int num;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    if(prime(num)){
+        printf("%d is Prime.\n", num);
+    } else {
+        printf("%d is Not Prime.\n", num);
+    }
+    return 0;
+}
